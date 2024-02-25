0e7a25a4d0c0ec2d64183ddb584aa8911b5da0bb
“Anmol” <pal592anmol@gmail.com>
2024-02-25 23:47:30 +0530

    Final config

--- webathon/settings.py
+++ webathon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_p%t$l^bbiwq$bps(r%=^n=9t%iz$57q(gqlr9hj&wf1k_=6lu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
