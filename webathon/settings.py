dbdeaefb145ee58fee60d370d16169ee46515ca4
“Anmol” <pal592anmol@gmail.com>
2024-02-26 19:11:51 +0530

    Final config

--- webathon/settings.py
+++ webathon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_p%t$l^bbiwq$bps(r%=^n=9t%iz$57q(gqlr9hj&wf1k_=6lu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'webathon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '4CBEaeE2D**bgdCGG1efCCgbd*1g5*bA',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '24577',
     }
 }
 
